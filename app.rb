88b6f0c4807c3aa82682363c61475c311f02311c
Valentina <maggio.vale@gmail.com>
2022-03-07 18:39:39 +0000

    Added params to return the name of the user
    Co-authored-by: Henry Ferrari <HCVF@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -17,7 +17,13 @@ get '/secretroom' do
   "That's a secret!"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ['Amigo', 'Misty', 'Almond'].sample
   erb(:index)
 end
 
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
